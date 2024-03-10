8aea6de56a322b03b26b4c43e605e05f3f2a4cef
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-10 20:46:49 +0100

    Initial Commit

---
+++ AFP/AFPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFPApp.swift
+//  AFP
+//
+//  Created by  Ixart on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AFPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
