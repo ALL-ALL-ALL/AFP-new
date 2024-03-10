5f5a53f7b2cf30e774805540e2249b1a44ddc666
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-10 21:34:16 +0100

    en cours ......

---
+++ AFP/DetailsUtilisateursView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsUtilisateursView.swift
+//  AFP
+//
+//  Created by  Ixart on 10/03/2024.
+//
+
+import SwiftUI
+
+struct DetailsUtilisateursView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsUtilisateursView()
+}
---
+++ AFP/UtiliView.swift
@@ -0,0 +1,18 @@
+//
+//  UtiliView.swift
+//  AFP
+//
+//  Created by  Ixart on 10/03/2024.
+//
+
+import SwiftUI
+
+struct UtiliView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UtiliView()
+}
