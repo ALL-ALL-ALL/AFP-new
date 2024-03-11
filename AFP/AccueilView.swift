a709c464aada79458df998225df8fb7a0a3843ee
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-11 21:12:39 +0100

    en cours....

---
+++ AFP/AccueilView.swift
@@ -0,0 +1,18 @@
+//
+//  AccueilView.swift
+//  AFP
+//
+//  Created by  Ixart on 11/03/2024.
+//
+
+import SwiftUI
+
+struct AccueilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccueilView()
+}
