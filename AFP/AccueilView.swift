062b0528ff2a59a45a79b9e4e1c79ebddc714463
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-12 01:42:27 +0100

    en cours (manque d'affectation des vues )

--- AFP/AccueilView.swift
+++ AFP/AccueilView.swift
@@ -11,61 +11,104 @@ import UIKit
 struct AccueilView: View {
     var body: some View {
 
-        VStack{
+        TabView {
+            VStack{
 
-            
-            Text("Apple Foundation Program")
-                .foregroundStyle(.gray)
-                .font(.title)
-            
-                .padding()
-            
-            Text("Bienvenue")
-                .bold()
-            
-                .padding()
-            
-            Image("R")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 160, height: 160)
-            
-                .padding()
-
-            
-            Image(systemName: "apple.logo")
-                .font(.system(size: 40))
-            
-            
-            
-            TabView{
-                Text("home")
-                    .tabItem {
-
-                       Label("Home", systemImage: "person.2.circle.fill")
-                        
-                                } // FIN tabitem
                 
+                Text("Apple Foundation Program")
+                    .foregroundStyle(.gray)
+                    .font(.title)
                 
-                Text("apple")
-                    .tabItem {
+                    .padding()
+                
+                Text("Bienvenue")
+                    .bold()
+                
+                    .padding()
+                
+                Image("R")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 160, height: 160)
+                
+                    .padding()
 
-                       Label("Home", systemImage: "apple.logo")
-                        
-                                } // FIN tabitem
                 
+                Image(systemName: "apple.logo")
+                    .font(.system(size: 40))
+                      
+                } // FIN vstack
+            
+            .tabItem { VStack{
                 
-                Text("groupes")
-                    .tabItem {
+                Image(systemName: "person.2.circle.fill")
+                Text("le Staff")
 
-                       Label("Home", systemImage: "sparkles.tv")
-                        
-                                } // FIN tabitem
+            } // fin vstack
+                
+ 
+                            } // FIN tabitem
+            
+            
+            
+            Text("AFP")
+                .tabItem { VStack{
+                    
+                    Image(systemName: "apple.logo")
+                    Text("AFP")
+                    
+                    
+                } // fin vstack
+                    
+                    
+        } // fin tabitem
+            
+            
+            Text("La promo")
+                .tabItem { VStack{
+                    
+                    Image(systemName: "sparkles.tv")
+                    Text("La promo")
+                    
+                    
+                } // fin vstack
+                    
+                    
+        } // fin tabitem
+            
+            
+            
+            
+            
+            
+            
             } // fin tabview
-         } // fin vstack
-    } //  fin body
-} // fin struct
+        
+        } // body
+    } // fin struct
 
 #Preview {
     AccueilView()
 }
+
+//TabView{
+//    Text("home")
+//        .tabItem {
+//
+//           Label("Home", systemImage: "person.2.circle.fill")
+//            
+//                    } // FIN tabitem
+//    
+//    
+//    Text("apple")
+//        .tabItem {
+//
+//           Label("Home", systemImage: "apple.logo")
+//            
+//                    } // FIN tabitem
+//    
+//    
+//    Text("groupes")
+//        .tabItem {
+//
+//           Label("Home", systemImage: "sparkles.tv")
--- AFP/LesGroupesView.swift
+++ AFP/LesGroupesView.swift
@@ -12,82 +12,119 @@ import UIKit
 struct LesGroupesView: View {
     
     var body: some View {
-        NavigationStack{
-            
-            
-            NavigationView {
-                   List(unGroupes) { groupe in
-                       ScrollView{
-                           NavigationLink {
-                               DetailsGroupesView(groupe: groupe)
-                              
+        TabView {
+            NavigationStack{
+                
+                
+                NavigationView {
+                       List(unGroupes) { groupe in
+                           ScrollView{
+                               NavigationLink {
+                                   DetailsGroupesView(groupe: groupe)
+                                  
+                                                   
+                                                   
                                                
-                                               
-                                           
-                              
-                               } label: {
-                               
-                               
-                               
-                               HStack {
+                                  
+                                   } label: {
                                    
                                    
                                    
-                                   VStack {
-                                       HStack {
-                                           Image(groupe.image1)
-                                           
-                                           Text(groupe.prenomAso)
-                                               .foregroundStyle(.black)
-                                               .bold()
-
-                                           Text(groupe.name1)
-                                               .foregroundStyle(.gray)
-
-                                          
-                                           
-                                           
-                                           
-                                           Spacer()
-                                           
-                                           Image(systemName: "arrow.right.to.line.circle.fill")
-                                               .foregroundColor(.red)
-                                               
-
-                                           
-                                           
-                                           
-                                       } // HASTACK
-
+                                   HStack {
                                        
+                                       
+                                       
+                                       VStack {
+                                           HStack {
+                                               Image(groupe.image1)
+                                               
+                                               Text(groupe.prenomAso)
+                                                   .foregroundStyle(.black)
+                                                   .bold()
+
+                                               Text(groupe.name1)
+                                                   .foregroundStyle(.gray)
+
+                                              
+                                               
+                                               
+                                               
+                                               Spacer()
+                                               
+                                               Image(systemName: "arrow.right.to.line.circle.fill")
+                                                   .foregroundColor(.red)
+                                                   
+
+                                               
+                                               
+                                               
+                                           } // HASTACK
+
+                                           
 
 
-                                   }// VSTACK
-                                   
-                               } // HSTACK
-                                   
-                           } // FIN NAVLINK
+                                       }// VSTACK
+                                       
+                                   } // HSTACK
+                                       
+                               } // FIN NAVLINK
+
+                               
+                           } // SCROOVIEW
 
                            
-                       } // SCROOVIEW
+                       } // FIN list
+                    
 
                        
-                   } // FIN list
+                    
+                       .listStyle(PlainListStyle()) // Pour que la liste prenne toute la largeur
+
+                    
+
+                    
+                   } // FIN NAVIGATIONVIEW
+                .navigationTitle("les Groupes")
+                
                 
 
-                   
+            } // FIN NAVIGATIONstack
+            .tabItem { VStack{
                 
-                   .listStyle(PlainListStyle()) // Pour que la liste prenne toute la largeur
+                Image(systemName: "person.2.circle.fill")
+                Text("le Staff")
 
+            } // fin vstack
                 
-
-                
-               } // FIN NAVIGATIONVIEW
-            .navigationTitle("les Groupes")
+ 
+                            } // FIN tabitem
+            
+            Text("AFP")
+                .tabItem { VStack{
+                    
+                    Image(systemName: "apple.logo")
+                    Text("AFP")
+                    
+                    
+                } // fin vstack
+                    
+                    
+        } // fin tabitem
             
             
-
-           } // FIN NAVIGATIONstack
+            
+            Text("La promo")
+                .tabItem { VStack{
+                    
+                    Image(systemName: "sparkles.tv")
+                    Text("La promo")
+                    
+                    
+                } // fin vstack
+                    
+                    
+        } // fin tabitem
+        } // FIN tabview
         
       
     } // fin body
