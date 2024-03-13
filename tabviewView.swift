208c7c7901c01a93876faff827bf9915d8a8d8f1
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-13 19:20:41 +0100

    finis

---
+++ tabviewView.swift
@@ -0,0 +1,18 @@
+//
+//  tabviewView.swift
+//  AFP
+//
+//  Created by  Ixart on 13/03/2024.
+//
+
+import SwiftUI
+
+struct tabviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tabviewView()
+}
